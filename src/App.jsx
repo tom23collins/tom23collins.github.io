5de5f55f68c5f983755946dc438ba72fd309a918
tom23collins <tom23collins@gmail.com>
2025-03-12 23:21:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 Welcome to My GitHub Page!</h1>
+      <p>This is a simple React app deployed on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
+
